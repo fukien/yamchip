56e70be20f0f5727b9341a66721e7a45c3db04d6
huangwentao <wentaohuang@ruc.edu.cn>
2018-06-17 23:18:45 +0800

    update new data

--- planB/js/startQuery.js
+++ planB/js/startQuery.js
@@ -14,49 +14,48 @@ function startQuery(){
 
 	// console.log("SENT DONE!");
 
-	// $.ajax({
-	// 	type: "GET",
-	// 	url: 'xml/main.xml',
-	// 	dataType:"xml",
-
-	// 	success : function(xml){
-	// 			alert('success');
-	// 			alert();
-	// 	},
-
-	// 	error: function(xml){
-	// 			alert("error" + data);
-	// 			return false;
-	// 	}
-
-	// });
-
 	$.ajax({
-			type: "GET",
-			url: 'data.json',
-			dataType: 'text',
-			success : function(data){
-				alert('success' + data);
-				alert(data);
-				config_data = $(data);
-				navigation_chosen_img = config_data.find("navigation_chosen_img").first();
-				alert("XJBC" + navigation_chosen_img);
-				navigation_unchosen_img = config_data.find("navigation_unchosen_img").first();
-				graph_type_chosen_img = config_data.find("graph_type_chosen_img").first();
-				graph_type_unchosen_img = config_data.find("graph_type_unchosen_img").first();	
-				graph_type = config_data.find("graph_type").first();
-				var graph_type_id = config_data.find("graph_type_id");	
-				for (var i = 0; i < graph_type_id.length; i++){
-					graph_type_ids[i] = graph_type_id[i].innerHTML;
-				}
-		//		alert(graph_type_ids);
-				element_init();
-			},
-			error: function(data)
-			{
-				alert("error" + data);
-			}
-		});
+		type: "GET",
+		url: 'data.txt',
+		dataType:"text",
 
+		success : function(data){
+				alert('success');
+				alert(data);
+		},
+
+		error: function(data){
+				alert("error" + data);
+				return false;
+		}
+
+	});
+
+	// $.ajax({
+	// 		type: "GET",
+	// 		url: 'data.json',
+	// 		dataType: 'text',
+	// 		success : function(data){
+	// 			alert('success' + data);
+	// 			alert(data);
+	// 			config_data = $(data);
+	// 			navigation_chosen_img = config_data.find("navigation_chosen_img").first();
+	// 			alert("XJBC" + navigation_chosen_img);
+	// 			navigation_unchosen_img = config_data.find("navigation_unchosen_img").first();
+	// 			graph_type_chosen_img = config_data.find("graph_type_chosen_img").first();
+	// 			graph_type_unchosen_img = config_data.find("graph_type_unchosen_img").first();	
+	// 			graph_type = config_data.find("graph_type").first();
+	// 			var graph_type_id = config_data.find("graph_type_id");	
+	// 			for (var i = 0; i < graph_type_id.length; i++){
+	// 				graph_type_ids[i] = graph_type_id[i].innerHTML;
+	// 			}
+	// 	//		alert(graph_type_ids);
+	// 			element_init();
+	// 		},
+	// 		error: function(data)
+	// 		{
+	// 			alert("error" + data);
+	// 		}
+	// 	});
 
 }
